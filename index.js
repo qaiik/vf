9eefef99dcbdb2ec28e8554add613807065da648
qaiik <selduaa2@gmail.com>
2022-11-30 17:11:01 -0700

    latest

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const app = require("express")();
 const path = require("path")
 
+app.get("/", (req,res) => {
+	res.sendFile(__dirname + "/flappy" + "/index.html")
+})
+
 app.get("*", (req,res) => {
   res.sendFile(path.join(__dirname, "flappy", req.path))
 })
